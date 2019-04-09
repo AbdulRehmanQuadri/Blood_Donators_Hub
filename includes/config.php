66ce8d3e3b4e01312afb58d65ccb0c0ef0b01cfb
Abdul Rehman <rehman.2511@yahoo.in>
2019-04-09 11:20:55 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,20 +1,22 @@
 <?php 
 // DB credentials.
 
-$DB_HOST = getenv('localhost');
-$DB_USER = getenv('root');
-$DB_PASS = getenv('');
-$DB_NAME = getenv('bbdms');
+$DB_HOST = getenv('db_host');
+$DB_USER = getenv('db_user');
+$DB_PASS = getenv('db_passwd');
+$DB_NAME = getenv('db_name');
 
 
-define('DB_HOST','localhost');
+/*define('DB_HOST','localhost');
 define('DB_USER','root');
 define('DB_PASS','');
-define('DB_NAME','bbdms');
+define('DB_NAME','bbdms');*/
 // Establish database connection.
 try
 {
-$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+//$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+$dbh = new PDO("mysql:host="$DB_HOST";dbname="$DB_NAME,$DB_USER, $DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+
 }
 catch (PDOException $e)
 {
